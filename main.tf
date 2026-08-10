37a1c1326ae5a49d411feb04cf9509de6e992b5f
Veer Kumar <veerkg11@gmail.com>
2026-08-10 23:36:36 +0530

    Initial Terraform commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "aptemp-terraform-cicd-2026"
+}
